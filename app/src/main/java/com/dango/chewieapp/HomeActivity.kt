d3185d6e309fc89a693161a5a152ba795fa4d7ff
Luis Villalobos <57929788+solidwolf@users.noreply.github.com>
2022-05-02 17:17:41 -0500

    SWCA-1 Added My Cards to menu

--- app/src/main/java/com/dango/chewieapp/HomeActivity.kt
+++ app/src/main/java/com/dango/chewieapp/HomeActivity.kt
@@ -43,7 +43,7 @@ class HomeActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_song_queue, R.id.nav_song_list, R.id.nav_leaderboard
+                R.id.nav_home, R.id.nav_song_queue, R.id.nav_song_list, R.id.nav_leaderboard, R.id.nav_my_cards
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
