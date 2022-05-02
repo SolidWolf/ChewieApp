ae6dad4947fc08051cc10855ef709042f7e3c21c
Luis Villalobos <57929788+solidwolf@users.noreply.github.com>
2022-05-02 17:01:47 -0500

    SWCA-1 Added Fragments, ViewModels, and Layouts to new Modules

---
+++ Leaderboard/src/main/java/com/dango/leaderboard/LeaderboardFragment.kt
@@ -0,0 +1,45 @@
+package com.dango.leaderboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.dango.leaderboard.databinding.FragmentLeaderboardBinding
+
+
+class LeaderboardFragment : Fragment() {
+
+    private lateinit var songListViewModel: LeaderboardViewModel
+    private var _binding: FragmentLeaderboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        songListViewModel =
+            ViewModelProvider(this).get(LeaderboardViewModel::class.java)
+
+        _binding = FragmentLeaderboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSlideshow
+        songListViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
