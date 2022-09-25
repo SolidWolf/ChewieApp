edb5ca432bff30177bcd37945cab2d1667be37d1
Luis Villalobos <57929788+solidwolf@users.noreply.github.com>
2022-09-25 18:13:09 -0500

    SW-3 added onclicklistener to buttons to open links

--- app/src/main/java/com/dango/chewieapp/home/HomeFragment.kt
+++ app/src/main/java/com/dango/chewieapp/home/HomeFragment.kt
@@ -1,21 +1,25 @@
 package com.dango.chewieapp.home
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.dango.chewieapp.HomeActivity
 import com.dango.chewieapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
+    private val GLORIOUSLEADER = "chewiemelodies"
+    private val TWITCHURL = "https://www.twitch.tv/"
+    private val YOUTUBEURL = "https://www.youtube.com/"
+    private val SPOTIFYURL = "https://open.spotify.com/artist/2dbjQX4XbIMrb5kayolqSZ?si=KsrM6Hn_SpiWj44_vPZ1kw"
+    private val PATREONURL = "https://www.patreon.com/"
+    private val DISCORDURL = "https://discordapp.com/invite/"
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,12 +35,39 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-
 //        val textView: TextView = binding.textHome
 //        homeViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClickListeners()
+    }
+
+    private fun onClickListeners(){
+        binding.twitchButton.setOnClickListener {
+            openApp(TWITCHURL+GLORIOUSLEADER)
+        }
+        binding.youtubeButton.setOnClickListener {
+            openApp(YOUTUBEURL+GLORIOUSLEADER)
+        }
+        binding.spotifyButton.setOnClickListener {
+            openApp(SPOTIFYURL)
+        }
+        binding.patreonButton.setOnClickListener {
+            openApp(PATREONURL+GLORIOUSLEADER)
+        }
+        binding.discordButton.setOnClickListener{
+            openApp(DISCORDURL+GLORIOUSLEADER)
+        }
+    }
+
+    private fun openApp(url: String){
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
